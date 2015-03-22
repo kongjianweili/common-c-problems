cfbe45a13ce25d29b761f66d97d63da2520b4996
wang_xw <634545374@qq.com>
2015-03-22 21:12:35 +0800

    何翔宇的问题

---
+++ mmap.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char buf[] = "hello world, hello world1234";
+	printf("1 failed\n");
+	int fd = open("a.txt", O_RDWR | O_CREAT, 0664);
+	printf("2 failed\n");
+	if(fd < 0)
+	{
+		printf("open failed\n");
+		return -1;
+	}
+	printf("3 failed\n");
+	char *addr = (char *)mmap(NULL, 1024, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+	printf("4 failed\n");
+	if(addr == (char *)-1)
+	{
+		printf("mmap failed\n");
+		return -1;
+	}
+	printf("5 %d  failed\n",strlen(buf));
+	memcpy(addr, buf, strlen(buf));
+	printf("6 failed\n");
+
+	munmap(addr, 1024);
+	printf("7 failed\n");
+
+	close(fd);
+
+	return 0;
+}
